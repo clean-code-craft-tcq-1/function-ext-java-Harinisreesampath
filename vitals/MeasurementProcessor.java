23f45efc63a1be001aa5dadc34882961b865c8d6
Harinisreesampath <30902956+Harinisreesampath@users.noreply.github.com>
2021-04-02 19:16:58 +0530

    Add files via upload

--- vitals/MeasurementProcessor.java
+++ vitals/MeasurementProcessor.java
@@ -8,29 +8,32 @@ public class MeasurementProcessor {
 	}
 	
 	 public String processedMessage(float value, float limitHolder[], String messageList[]){
+		 String selectedMessage = "";
 			if(value > limitHolder[0] && value <= limitHolder[1]) {
 				this.status = false;
-				return messageList[0];	
+				selectedMessage = messageList[0];	
 				}
 			
-			if(value > limitHolder[1] && value <= limitHolder[2]) {
-			  return messageList[1];
+			else if(value > limitHolder[1] && value <= limitHolder[2]) {
+				selectedMessage = messageList[1];
 			  }
 			
-			if(value > limitHolder[2] && value <= limitHolder[3]) {
-			  return messageList[2];
+			else if(value > limitHolder[2] && value <= limitHolder[3]) {
+				selectedMessage = messageList[2];
 			  }
 			
-			if(value > limitHolder[3] && value <= limitHolder[4]) {
-			  return messageList[3];
+			else if(value > limitHolder[3] && value <= limitHolder[4]) {
+				selectedMessage = messageList[3];
 			  }
 			
-			if(value > limitHolder[4]) {
+			else if(value > limitHolder[4]) {
 				this.status = false;
-				return messageList[4];
+				selectedMessage = messageList[4];
 				}		  
+			else
+				selectedMessage = null;
 			
-			return null;
+			return selectedMessage;
 			
 			}
 			
