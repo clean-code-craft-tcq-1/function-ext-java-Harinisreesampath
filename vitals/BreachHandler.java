f89bc383989d2f416d67fd6fd808e869bcf96a85
Harinisreesampath <30902956+Harinisreesampath@users.noreply.github.com>
2021-04-08 17:10:25 +0530

    Update BreachHandler.java

--- vitals/BreachHandler.java
+++ vitals/BreachHandler.java
@@ -3,7 +3,6 @@ package vitals;
 public class BreachHandler {
 
 	static void isActionNeeded(boolean measurementStatus, String breachedMeasurement) {
-		System.out.println("measurementStatus : "+ measurementStatus);
 		if(!measurementStatus) {
 			System.out.println("Taking action for breached "+ breachedMeasurement);
 		}
